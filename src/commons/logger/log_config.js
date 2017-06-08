f6970901c3702c35db4b2c43f1809e05392ae8e3
GuleriaVarun <cse2011.varun@gmail.com>
2017-06-08 11:46:29 +0530

    Added winston-loglevel and removed modified timestamp for better performance

--- src/commons/logger/log_config.js
+++ src/commons/logger/log_config.js
@@ -20,63 +20,50 @@ var logConf = {
       name: 'info-file',
       filename: './logs/filelog-info.log',
       maxsize: '2000',
+      levelOnly: false, // if true, will only log the specified level, if false will log from the specified level and above 
       maxFiles: 2,
       json: false,
-      timestamp: function () {
-        return new Intl.DateTimeFormat("en-US").format(new Date()) + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "-" + new Date().getMilliseconds()
-      },
       level: 'info'
     }),
     new (winston.transports.File)({
       name: 'error-file',
       filename: './logs/filelog-error.log',
       maxsize: '2000',
+      levelOnly: false,
       maxFiles: 2,
       json: false,
-      timestamp: function () {
-        return new Intl.DateTimeFormat("en-US").format(new Date()) + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "-" + new Date().getMilliseconds()
-      },
       level: 'error'
     }),
     new (winston.transports.File)({
       name: 'debug-file',
       filename: './logs/filelog-debug.log',
       maxsize: '2000',
+      levelOnly: false,
       maxFiles: 2,
       json: false,
-      timestamp: function () {
-        return new Intl.DateTimeFormat("en-US").format(new Date()) + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "-" + new Date().getMilliseconds()
-      },
       level: 'debug'
     }),
     new (winston.transports.File)({
       name: 'warn-file',
       filename: './logs/filelog-warn.log',
       maxsize: '2000',
+      levelOnly: false,
       maxFiles: 2,
       json: false,
-      timestamp: function () {
-        return new Intl.DateTimeFormat("en-US").format(new Date()) + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "-" + new Date().getMilliseconds()
-      },
       level: 'warn'
     }),
     new (winston.transports.File)({
       name: 'trace-file',
       filename: './logs/filelog-trace.log',
       maxsize: '2000',
+      levelOnly: false,
       maxFiles: 2,
       json: false,
-      timestamp: function () {
-        return new Intl.DateTimeFormat("en-US").format(new Date()) + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "-" + new Date().getMilliseconds()
-      },
       level: 'silly'
     })
   ],
   exceptionHandlers: [
       new winston.transports.File({
-        timestamp: function () {
-          return new Intl.DateTimeFormat("en-US").format(new Date()) + " " + new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds() + "-" + new Date().getMilliseconds()
-        },
         filename: './logs/exceptions.log',
         json: false,
         maxsize: '2000',
