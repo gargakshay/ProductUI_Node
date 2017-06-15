b1882cd881421e97bff145e7840b88a9c14faacd
GuleriaVarun <cse2011.varun@gmail.com>
2017-06-15 12:54:33 +0530

    Added module entry for routing

--- src/routes/webdashboard/services/index.js
+++ src/routes/webdashboard/services/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 
 var treeService = require('./tree-service');
+var initWebdashboard = require('./init-webdashboard');
+var favoriteService = require('./favorite-data-services');
+var dashboardDataService = require('./dashboard-data-service-resource');
 
 var router = express.Router();
 
 router.use("/DashboardServer/web/", treeService);
+
+router.use("/DashboardServer/web/", initWebdashboard);
+
+router.use("/DashboardServer/web/", favoriteService);
+
+router.use("/DashboardServer/web/", dashboardDataService);
 
 module.exports = router;
