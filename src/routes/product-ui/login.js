47882186a366008baa818d9a2cf9ccb3813a383e
GuleriaVarun <cse2011.varun@gmail.com>
2017-06-15 12:53:13 +0530

    removed unused code for middleware

--- src/routes/product-ui/login.js
+++ src/routes/product-ui/login.js
@@ -5,57 +5,10 @@ var async = require('async');
 
 var URL = require('../../constants/product-ui-url');
 var log = require('../../commons/logger/logger');
-var restCall = require('../../commons/rest-api/rest-call');
 var dcinfo = require('../../../conf/dcinfo');
 
 var router = express.Router();
 var _filename = "login";
-
-router.use((req, res, next) => {
-
-    try {
-        // Get DCName from baseurl (/node/:dc)
-        var dcname = req.baseUrl.substring(6);
-        var path = req.url;
-        var loaders;
-
-        // In case of requested DC is ALL then we need to merge all DC data.
-        if (dcname && dcname.toUpperCase() === 'ALL') {
-
-            // Creating callback for each DC's request to be feed by async.parallel
-            loaders = dcinfo.map(function (dcobj) {
-                let url = `${req.protocol}://${dcobj.ip}:${dcobj.port}${path}`;
-
-                return function (callback) {
-                    restCall.getRequest(url, dcobj.dc, callback);
-                };
-            });
-        }
-        // For Individual DC request
-        else {
-            // Find DC Obj
-            var dcobj = dcinfo.find((info) => {
-                return info.dc === dcname;
-            });
-
-            let url = `${req.protocol}://${dcobj.ip}:${dcobj.port}${path}`;
-
-            loaders = [function (callback) {
-                restCall.getRequest(url, dcname, callback)
-            }];
-        }
-
-        async.parallel(loaders, (err, results) => {
-            // Set respond data in req. object
-            req.data = results[0];
-            next();
-        });
-    }
-    catch (e) {
-        log.error(_filename, "router.use", e);
-        next();
-    }
-});
 
 /** Get Product Name */
 router.get(URL.PRODUCT_NAME, getData);
@@ -105,6 +58,8 @@ router.get(URL.PROJ_SUBPROJ_LIST, getData);
 /**Get Proj Sub Proj Scenario Data */
 router.get(URL.PROJ_SUBPROJ_SCENARIO_LIST, getData);
 
+/**Set Sceanrio Logging Info */
+router.get(URL.SET_LOGGING_INFO, getData);
 
 /** Using for get directly data from NDE node */
 function getData(req, res, next) {
@@ -123,6 +78,5 @@ function getDCInfo(req, res) {
 process.on('uncaughtException', (err) => {
   console.log(err);
 });
-
 
 module.exports = router;
